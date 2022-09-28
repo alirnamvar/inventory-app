58becb5741aeb251afaf065e026cb5fc21c60778
alirnamvar <alirnamvar1999@gmail.com>
2022-09-28 22:56:18 +0330

    add come Color constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,10 +4,17 @@ import 'package:rflutter_alert/rflutter_alert.dart';
 const Color kAppBarBackgroundColor = Color(0xFF0A0E25);
 const Color kScaffoldBackgroundColor = Color(0xFF0A0E21);
 const Color kElevatedButtonPrimaryColor = Color(0xFF000062);
-const Color kPalletNotSelected = Color(0xFF1D1E33);
+const Color kNotSelectedPallet = Color(0xFF1D1E33);
+const Color kSelectedPallet = Color(0xFF111328);
 
 const TextStyle kButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
+);
+
+const TextStyle kNumberInventoryPage = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 10.0,
+  color: Color(0xFF8D8E98),
 );
 
 const AlertStyle kAlertStyle = AlertStyle(
